2b4399b02f1a87b58c95cc7e2d0645faa2b05391
Nikolas Rimikis <nikolas.rimikis@debacode.de>
2026-03-27 18:05:58 +0100

    fix: expose StreamingClassificationResult
    Signed-off-by: Nikolas Rimikis <nikolas.rimikis@debacode.de>

--- packages/pose_classifier/lib/pose_classifier.dart
+++ packages/pose_classifier/lib/pose_classifier.dart
@@ -1,6 +1,6 @@
 export 'src/classification_result.dart';
 export 'src/ema_smoothing.dart';
-export 'src/pose_classification_result.dart' show PoseClassificationResult;
+export 'src/pose_classification_result.dart';
 export 'src/pose_classifier.dart';
 export 'src/pose_classifier_processor.dart';
 export 'src/pose_embedding.dart';
