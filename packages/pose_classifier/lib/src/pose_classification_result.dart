3827386cbbd34c68735aca9b3973fe892f6c17b7
Nikolas Rimikis <nikolas.rimikis@debacode.de>
2026-03-27 15:13:14 +0100

    refactor: split streaming processor
    Signed-off-by: Nikolas Rimikis <nikolas.rimikis@debacode.de>

--- packages/pose_classifier/lib/src/pose_classification_result.dart
+++ packages/pose_classifier/lib/src/pose_classification_result.dart
@@ -1,29 +1,53 @@
-typedef Repetition = ({String name, int reps});
-typedef PoseConfidence = ({String? name, double confidence});
-
+/// Result of a pose classification.
 class PoseClassificationResult {
-  PoseClassificationResult({
-    required Repetition? repetitions,
-    required this.lastPose,
-  }) : repetitionClass = repetitions?.name,
-       repetitionCount = repetitions?.reps;
+  /// Creates a new pose classification result.
+  const PoseClassificationResult({
+    required this.className,
+    required this.confidence,
+  });
 
-  /// The detected pose repetition.
-  final String? repetitionClass;
+  /// The class of classified pose.
+  final String? className;
 
-  /// The number of repetitions.
-  final int? repetitionCount;
-
-  /// The last detected pose with its confidence.
-  final PoseConfidence? lastPose;
+  /// The confidence of the classified [className].
+  final double? confidence;
 
   @override
   String toString() {
     return '''
 PoseClassificationResult(
+  className: $className,
+  confidence: $confidence,
+)
+''';
+  }
+}
+
+/// Result of a streaming pose classification.
+
+class StreamingClassificationResult extends PoseClassificationResult {
+  /// Creates a new pose classification result for a streaming processor.
+  const StreamingClassificationResult({
+    required this.repetitionClass,
+    required this.repetitionCount,
+
+    super.className,
+    super.confidence,
+  });
+
+  /// The detected pose repetition.
+  final String? repetitionClass;
+
+  /// The number of repetitions of the detected [repetitionClass].
+  final int? repetitionCount;
+
+  @override
+  String toString() {
+    return '''
+StreamingClassificationResult(
   repetitionClass: $repetitionClass,
   repetitionCount: $repetitionCount,
-  lastPose: $lastPose,
+  lastPose: ($className, $confidence),
 )
 ''';
   }
--- packages/pose_classifier/lib/src/pose_classifier_processor.dart
+++ packages/pose_classifier/lib/src/pose_classifier_processor.dart
@@ -7,30 +7,47 @@ import 'pose_classifier.dart';
 import 'pose_sample.dart';
 import 'repetition_counter.dart';
 
-/// A processor that loads pose samples and classifies incoming poses.
-///
-/// This class is designed to handle pose classification and repetition
-/// counting. It is important to call `getPoseResult` off the UI thread
-/// if heavy processing is involved, as Flutter isolates apply.
-///
-/// Accepts a stream of [mlkit.Pose] for classification and repetition
-/// counting.
+/// A processor that classifies incoming poses.
 class PoseClassifierProcessor {
   /// Creates an instance of [PoseClassifierProcessor].
   ///
   /// The [poseSamples] parameter is required and should contain the
   /// samples used for pose classification.
   ///
-  /// Use [PoseClassifierProcessor.streaming] for EMA smoothing and repetition
+  /// Use [StreamingClassifierProcessor] for EMA smoothing and repetition
   /// counting.
   PoseClassifierProcessor({required List<PoseSample> poseSamples})
-    : _poseClassifier = PoseClassifier(poseSamples),
-      _isStreamMode = false,
-      _emaSmoothing = null,
-      _repCounters = null,
-      _className = null;
+    : _poseClassifier = PoseClassifier(poseSamples);
 
-  /// Creates a streaming instance of [PoseClassifierProcessor].
+  final PoseClassifier _poseClassifier;
+
+  /// Given a new [mlkit.Pose] input, returns the classified pose.
+  PoseClassificationResult classifyPose(mlkit.Pose pose) {
+    final classification = _poseClassifier.classifyFromPose(pose);
+
+    // Add maxConfidence class of current frame to result if pose is found.
+    final maxConfidenceClass = classification.getMaxConfidenceClass();
+    final maxConfidence =
+        classification.getClassConfidence(maxConfidenceClass) /
+        _poseClassifier.confidenceRange();
+
+    return PoseClassificationResult(
+      className: maxConfidenceClass,
+      confidence: maxConfidence,
+    );
+  }
+}
+
+/// A processor that loads pose samples and classifies incoming poses.
+///
+/// This class is designed to handle pose classification and repetition
+/// counting. It is important to call [classifyPose] off the UI thread
+/// if heavy processing is involved, as Flutter isolates apply.
+///
+/// Accepts pose from a stream of [mlkit.Pose] for classification and repetition
+/// counting.
+class StreamingClassifierProcessor extends PoseClassifierProcessor {
+  /// Creates a streaming instance of [StreamingClassifierProcessor].
   ///
   /// The [poseSamples] parameter is required and should contain the
   /// samples used for pose classification.
@@ -38,19 +55,14 @@ class PoseClassifierProcessor {
   ///
   /// This instance enables EMA smoothing and repetition counting.
   /// Specify [className] to
-  PoseClassifierProcessor.streaming({
-    required List<PoseSample> poseSamples,
-    String? className,
-  }) : _isStreamMode = true,
-       _poseClassifier = PoseClassifier(poseSamples),
-       _emaSmoothing = EMASmoothing(),
-       _repCounters = _repCounterFromSamples(poseSamples),
-       _className = className;
 
-  final bool _isStreamMode;
-  final EMASmoothing? _emaSmoothing;
-  final Map<String, RepetitionCounter>? _repCounters;
-  final PoseClassifier _poseClassifier;
+  StreamingClassifierProcessor({required super.poseSamples, String? className})
+    : _emaSmoothing = EMASmoothing(),
+      _repCounters = _repCounterFromSamples(poseSamples),
+      _className = className;
+
+  final EMASmoothing _emaSmoothing;
+  final Map<String, RepetitionCounter> _repCounters;
   final String? _className;
 
   /// Creates repetition counters for all available pose types.
@@ -70,50 +82,43 @@ class PoseClassifierProcessor {
   }
 
   // Cache last result if the current pose is empty.
-  Repetition? _repetitionCounters;
+  ({String name, int reps})? _repetitionCounter;
 
-  /// Given a new [mlkit.Pose] input, returns a Map of formatted String
-  /// with pose classification results.
-  ///
-  /// The result contains up to two strings:
-  /// 0: PoseClass : X reps
-  /// 1: PoseClass : [0.0-1.0] confidence
+  /// Given a new [mlkit.Pose] input, returns the classified pose.
   ///
   /// Returns the repetition class with the highest wrap count.
   /// Specify poseType in the constructor to only track one type.
-  PoseClassificationResult? getPoseResult(mlkit.Pose pose) {
+  @override
+  StreamingClassificationResult classifyPose(mlkit.Pose pose) {
     // Return early without updating repCounter if no pose found.
     if (pose.landmarks.isEmpty) {
-      return PoseClassificationResult(
-        repetitions: _repetitionCounters,
-        lastPose: null,
+      return StreamingClassificationResult(
+        repetitionClass: _repetitionCounter?.name,
+        repetitionCount: _repetitionCounter?.reps,
       );
     }
 
     var classification = _poseClassifier.classifyFromPose(pose);
 
     // Update repetitionCounters if in stream mode.
-    if (_isStreamMode) {
-      // Feed pose to smoothing even if no pose found.
-      classification = _emaSmoothing!.getSmoothedResult(classification);
+    // Feed pose to smoothing even if no pose found.
+    classification = _emaSmoothing.getSmoothedResult(classification);
+    if (_className != null) {
+      final repCounter = _repCounters[_className];
+      final repsAfter =
+          repCounter?.addClassificationResult(classification) ?? 0;
 
-      if (_className == null) {
-        for (final repCounter in _repCounters!.values) {
-          final repsBefore = repCounter.numRepeats;
-          final repsAfter = repCounter.addClassificationResult(classification);
+      _repetitionCounter = (name: _className, reps: repsAfter);
+    } else {
+      for (final repCounter in _repCounters.values) {
+        final repsBefore = repCounter.numRepeats;
+        final repsAfter = repCounter.addClassificationResult(classification);
 
-          // First pose we find a repetition for must be the right one.
-          if (repsAfter > repsBefore) {
-            _repetitionCounters = (name: repCounter.className, reps: repsAfter);
-            break;
-          }
+        // First pose we find a repetition for must be the right one.
+        if (repsAfter > repsBefore) {
+          _repetitionCounter = (name: repCounter.className, reps: repsAfter);
+          break;
         }
-      } else {
-        final repCounter = _repCounters![_className];
-        final repsAfter =
-            repCounter?.addClassificationResult(classification) ?? 0;
-
-        _repetitionCounters = (name: _className, reps: repsAfter);
       }
     }
 
@@ -123,9 +128,12 @@ class PoseClassifierProcessor {
         classification.getClassConfidence(maxConfidenceClass) /
         _poseClassifier.confidenceRange();
 
-    return PoseClassificationResult(
-      repetitions: _repetitionCounters,
-      lastPose: (name: maxConfidenceClass, confidence: maxConfidence),
+    return StreamingClassificationResult(
+      repetitionClass: _repetitionCounter?.name,
+      repetitionCount: _repetitionCounter?.reps,
+
+      className: maxConfidenceClass,
+      confidence: maxConfidence,
     );
   }
 }
